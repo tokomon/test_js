39429086c781996e8c5806da74f32f7246b5bed7
Gerson Aduviri <gerson@geekadvice.pe>
2017-06-23 12:27:13 -0500

    Pruebas unitarias y trabajo con objetos

--- index.js
+++ index.js
@@ -1,23 +1,83 @@
 var assert = require('assert');
 
-function suma(op1, op2)
+
+
+function EjerciciosArray()
 {
-  if(!op2)
+  this.ejercicio1 = function(_arr, _num)
   {
-    op2 = 0;
+      var res = 0;
+      if(Array.isArray(_arr))
+      {
+        res = _arr.reduce(function(_total, _actual){
+          return _total * _actual;
+        });
+      }else{
+        if(!isNaN(_arr)){
+          res = _arr;
+        }
+      }
+
+
+     if(!isNaN(_num)){
+       res = res * _num;
+     }
+
+     return res;
   }
-  return op1 + op2;
+
+  this.ejercicio2 = function(_telefonos, _filtro)
+  {
+      return _telefonos.filter(function(a){
+        var primeras = a.toString().substr(0,3);
+        var ultimas = a.toString().substr(-3);
+        return (primeras == _filtro || ultimas == _filtro);
+      });
+  }
 }
 
 
-describe('Función suma', function(){
-  it('5 de la suma de 3 y 2', function(){
-    assert.equal(5, suma(3,2));
+var ejercicios = new EjerciciosArray();
+
+
+
+
+describe('Ejercicios Arrays', function(){
+  describe('Ejercicio 1', function(){
+    it('Prueba de [2,3] y 10', function(){
+      assert.equal(60, ejercicios.ejercicio1([2,3], 10));
+    });
+    it('Prueba de [2,3] y "Hola"', function(){
+      assert.equal(6, ejercicios.ejercicio1([2,3], "Hola"));
+    });
+    it('Prueba de [2,3] y nada', function(){
+      assert.equal(6, ejercicios.ejercicio1([2,3]));
+    });
+    it('Prueba de 2 y 3', function(){
+      assert.equal(6, ejercicios.ejercicio1(2,3));
+    });
   });
-  it('3 de la suma de 3 y nada', function(){
-    assert.equal(3, suma(3));
+
+  describe('Ejercicio 2', function(){
+    it("cuando es [996548337, 997616207], 207", function(){
+      assert.deepEqual([997616207], ejercicios.ejercicio2([996548337, 997616207], 207));
+    });
+    it("cuando es [996548337, 97616207], 207", function(){
+      assert.deepEqual([97616207], ejercicios.ejercicio2([996548337, 97616207], 207));
+    });
   });
-  it('3 de la suma de nada y 3', function(){
-    assert.equal(3, suma("null", 3));
-  });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//
